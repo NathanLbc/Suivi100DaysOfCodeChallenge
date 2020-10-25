f5b04fcf32398f8567ccdccc3518c39b00d3a562
NathanLbc <38780018+NathanLbc@users.noreply.github.com>
2020-10-26 00:37:53 +0100

    Udpate title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   ssr: false,
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'mapReduceFilter',
+    title: '100DaysOfCode - Nathan Lambec',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
